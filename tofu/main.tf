585ed8ef4615a0ebf779f9ac1b6a579f3dd9034d
James Roberts <jrwroberts1976@gmail.com>
2026-08-29 21:44:29 +0100

    Start disposable IaC proof VM

--- tofu/main.tf
+++ tofu/main.tf
@@ -21,7 +21,7 @@ resource "proxmox_virtual_environment_vm" "debian_iac_test" {
   node_name = var.node_name
   vm_id     = var.vm_id
 
-  started         = false
+  started         = true
   on_boot         = false
   stop_on_destroy = true
 
